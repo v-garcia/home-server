548de400d66cdad61ebee2362929744c7af5652d
Vincent <vincent@brix-vincent.localdomain>
2020-03-03 08:52:47 +0100

    couchdb

---
+++ apps/gerbera/test-ctn/test-ctn.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+docker build --rm -t  gerbera ../
+docker run --net=host \
+    -v $(pwd)/home/:/root/ \
+    -v $(pwd)/config.xml/:/root/.config/gerbera/config.xml:ro \
+    -v /data/ctorrent/downloads:/media \
+    gerbera
+
+ # -v /data/ctorrent/downloads:/media/video \
