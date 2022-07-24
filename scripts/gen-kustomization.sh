59b572726f8e8ab8b517453ff10886ee03e528a6
Vincent Garcia <VGarcia@sierrawireless.com>
2022-07-24 12:03:32 +0200

    Update gen-customization.sh

--- scripts/gen-kustomization.sh
+++ scripts/gen-kustomization.sh
@@ -10,11 +10,11 @@ cd ../apps/$APP_NAME
 cp -nv $SCRIPT_DIR/templates/kustomization-base.yaml ./kustomization.yaml
 
 # reseting resources
-yq w -i ./kustomization.yaml resources ''
+yq -i '.resources=[]' ./kustomization.yaml
 
 # adding all resources to reources field
 for f in ./$APP_NAME-*.yaml; do
     fname=$(basename $f)
     echo "Adding resources -> $fname"
-    [[ ! -z "$fname" ]] && yq w -i ./kustomization.yaml resources[+] "$fname"
+    [[ ! -z "$fname" ]] && yq -i ".resources += \"$fname\"" ./kustomization.yaml
 done
