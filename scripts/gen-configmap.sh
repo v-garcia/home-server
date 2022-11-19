51c7c34fa4fd3930bce54bdfe97ac6ce01952c2f
Vincent Garcia <mondaying@hotmail.com>
2022-11-19 12:26:20 +0100

    Normalize config files

--- scripts/gen-configmap.sh
+++ scripts/gen-configmap.sh
@@ -2,4 +2,6 @@
 
 # usage ./gen-configmap.sh minidlna ./apps/minidlna/test-ctn/minidlna.conf ./apps/minidlna/
 
+sed -i 's/\t/    /g' "$2"
+sed -i -E 's/[[:space:]]+$//g' "$2"
 kubectl create configmap "$1" --from-file="$2" --dry-run  -o yaml > "$3/$1-cm.yaml"
